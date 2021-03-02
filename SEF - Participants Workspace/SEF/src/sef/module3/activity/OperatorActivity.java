2e54cc30a538435d849b172dd568391412186b1f
Jevgenija Bobrova <jevgenija.bobrova@gmail.com>
2021-03-02 10:46:39 +0200

    Operator Activity

--- SEF - Participants Workspace/SEF/src/sef/module3/activity/OperatorActivity.java
+++ SEF - Participants Workspace/SEF/src/sef/module3/activity/OperatorActivity.java
@@ -22,10 +22,10 @@ public class OperatorActivity {
 		System.out.println("Difference = " + result);
 		
 		// Add numbers
-		
+		int sumResult = i +j;
 		
 		// Print result
-		
+		System.out.println("Summ = " + sumResult);
 		
 	}
 
