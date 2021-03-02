98a4d8f5f28e3e7eb83d2f73ac7cca8dd0c892ce
Jevgenija Bobrova <jevgenija.bobrova@gmail.com>
2021-03-02 16:37:43 +0200

    Module 4

---
+++ SEF - Participants Workspace/SEF/src/sef/module4/activity/DateActivityMyself.java
@@ -0,0 +1,14 @@
+package activity;
+
+import java.util.Date;
+
+
+public class DateActivityMyself {
+
+    public static void main(String[] args) {
+            Date today = new Date();
+            System.out.println(today);
+        }
+
+    }
+
