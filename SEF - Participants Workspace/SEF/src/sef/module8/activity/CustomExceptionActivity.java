e8603638c4144c7a1e80b9a55169b1ab4250d44a
Jevgenija Bobrova <jevgenija.bobrova@gmail.com>
2021-03-03 20:00:20 +0200

    Module 8 activity 4

--- SEF - Participants Workspace/SEF/src/sef/module8/activity/CustomExceptionActivity.java
+++ SEF - Participants Workspace/SEF/src/sef/module8/activity/CustomExceptionActivity.java
@@ -1,13 +1,14 @@
 package activity;
+
 //Needs to be completed
 //1 - extend this class from Exception class
-public class CustomExceptionActivity extends Exception{
+public class CustomExceptionActivity extends Exception {
 
-	//2 - Override getMessage() and provide the following message 
-	// "You are not allowed to use this program"
-	public String getMessage(){
-		System.out.println("You are not allowed to use this program");
-	    return null;
+    //2 - Override getMessage() and provide the following message
+    // "You are not allowed to use this program"
+    public String getMessage() {
+        System.out.println("You are not allowed to use this program");
+        return null;
     }
-	
+
 }
