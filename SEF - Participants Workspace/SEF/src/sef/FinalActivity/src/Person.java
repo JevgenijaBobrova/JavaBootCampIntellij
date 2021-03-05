39aaf0fa515424400c47bdb87de17107218da39e
Jevgenija Bobrova <jevgenija.bobrova@gmail.com>
2021-03-05 09:43:38 +0200

    FinalActivity

---
+++ SEF - Participants Workspace/SEF/src/sef/FinalActivity/src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    String name;
+    int age;
+
+    public void introducePerson(String name, int age) {
+        System.out.println("My name is " + name + " and I am " + age + " years old");
+    }
+}
+
